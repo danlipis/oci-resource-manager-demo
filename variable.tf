7f9697b88262faeb81a27fd2d65d302906ae0864
danlipis <daniel.cgid@gmail.com>
2021-09-09 16:38:01 -0300

    add new file

---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "compartment_ocid" {
+    default = "ocid1.compartment.oc1..aaaaaaaaatvqndqfls2ufvtsrwff7ikepddfj4ks3okdyytqcmymqsqk6rpa"
+}
+variable "region" {
+    default = "us-ashburn-1"
+}
