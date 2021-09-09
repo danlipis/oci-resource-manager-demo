556502a6a9734722a6fd944d7c64ad601d8411cd
Daniel dos Santos Bastos <daniel.cgid@gmail.com>
2021-09-09 15:39:45 -0300

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,20 +1,82 @@
-variable "bucket_namespace" {
-    default = "toddrsharp"
-}
-variable "bucket_name" {
-    default = "resource_manager_demo_bucket"
-}
+## Copyright © 2020, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-resource "oci_objectstorage_bucket" "create_bucket" {
-    # required
-    compartment_id = var.compartment_ocid
-    name = var.bucket_name
-    namespace = var.bucket_namespace
+# Create VCN
 
-    # optional
-    access_type = "NoPublicAccess" # <---- updated from "ObjectRead"
+resource "oci_core_virtual_network" "vcn" {
+  cidr_block = "10.0.0.0/16"
+  compartment_id = var.compartment_ocid
+  display_name = "demo-vcn"
+  dns_label      = "tfexamplevcn"
 }
 
-output "new_bucket" {
-    value = oci_objectstorage_bucket.create_bucket
+# Create internet gateway to allow public internet traffic
+
+resource "oci_core_internet_gateway" "ig" {
+  compartment_id = var.compartment_ocid
+  display_name = "internet-gateway"
+  vcn_id = oci_core_virtual_network.vcn.id
+}
+
+# Create route table to connect vcn to internet gateway
+
+resource "oci_core_route_table" "rt" {
+  compartment_id = var.compartment_ocid
+  vcn_id = oci_core_virtual_network.vcn.id
+  display_name = "rt-table"
+  route_rules {
+    cidr_block = "0.0.0.0/0"
+    network_entity_id = oci_core_internet_gateway.ig.id
+  }
+}
+
+# Create security list to allow internet access from compute and ssh access
+
+resource "oci_core_security_list" "sl" {
+  compartment_id = var.compartment_ocid
+  display_name = "security-list"
+  vcn_id = oci_core_virtual_network.vcn.id
+  
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol = "6"
+  }
+
+  ingress_security_rules {
+
+    protocol = "6"
+    source = "0.0.0.0/0"
+
+    tcp_options {
+      max = 22
+      min = 22
+    }
+}
+
+ingress_security_rules {
+    protocol = "6"
+    source = "0.0.0.0/0"
+
+    tcp_options {
+      max = 80
+      min = 80
+    }
+  }
+}
+
+# Create regional subnet in vcn
+
+resource "oci_core_subnet" "subnet_1" {
+  cidr_block = "10.0.1.0/24"
+  display_name = "subnet-1"
+  compartment_id = var.compartment_ocid
+  vcn_id = oci_core_virtual_network.vcn.id
+  dhcp_options_id = oci_core_virtual_network.vcn.default_dhcp_options_id
+  route_table_id = oci_core_route_table.rt.id
+  security_list_ids = [oci_core_security_list.sl.id]
+  dns_label         = "subnet"
+
+  provisioner "local-exec" {
+    command = "sleep 5"
+  }
 }
